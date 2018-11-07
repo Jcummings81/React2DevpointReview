2e33dd5fd395cf99d1427e3ffd469b64bbf2853d
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-11-07 15:28:59 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, name: 'Learn Rails', complete: true },
+      {id: 2, name: 'Learn React', complete: false },
+      {id: 3, name: 'Learn Redux', complete: false },
+      {id: 4, name: 'Profit', complete: false }
+    ]
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+return ( 
+    <div>
+      <ul>
+        { this.state.todos.map( todo => {
+            return <li key={todo.id}>{todo.name}</li>
+        } 
+        )}
+        
+      </ul>
+    </div>
     );
   }
 }
