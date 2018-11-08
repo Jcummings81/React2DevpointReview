9fb1decfa498723b81d43fcb2f5033079f647a11
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-11-08 08:33:31 -0700

    completed Clock component

--- src/Clock.js
+++ src/Clock.js
@@ -1,5 +1,26 @@
 import React from 'react'
 
-const Clock = () => <div>Clock</div>
+class Clock extends React.Component {
+    state = { time: new Date() }
+
+    componentDidMount() {
+        this.ticker = setInterval( this.tick, 1000 )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.ticker)
+    }
+
+    tick = () => {
+        this.setState({ time: new Date() })
+    }
+    render() {
+        return (
+            <h1>
+                { this.state.time.toLocaleTimeString() }
+            </h1>
+        )
+    }
+}
 
 export default Clock
